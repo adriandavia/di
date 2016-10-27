4f5ad8d093740797bb574172dd1223ee699cb4b5
adriandavia <dam.adavia@institutoserpis.org>
2016-10-27 09:49:26 +0200

    Ejercicios Python Listas 2

--- Python/EjerciciosPythonListas2/Ej3.py
+++ Python/EjerciciosPythonListas2/Ej3.py
@@ -3,13 +3,20 @@
 
 numero = int (input("Numero: "))
 lista1 = []
-i = 1
-x = 0
-cont = 0
-while (i <= numero):
-	if (numero % i == 0):
-		cont = 0
-	else:
-		i += 1 
 
-print (lista1)
+y = 0 #Para rellenar lista 
+for a in range(numero+1):
+	x = 0 #contador
+	i = 1 #calcular divisores
+	while (i <= a):
+		if (a % i == 0):
+			x += 1
+			i += 1 
+		else:
+			i += 1 
+	if (a != 0):
+		if (x <= 2):
+			lista1[0:y] += [str(a)]
+			y += 1
+
+print ("Numeros primos desde 0 hasta el", numero, ":",lista1)
